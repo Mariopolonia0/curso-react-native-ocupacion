7b1e8d36c15fe9a0dbdb806cec039d76c052286a
mario <mariopolonia0@gmil.com>
2025-01-12 22:28:45 -0500

    agregando conexion a api rest

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { getBooks } from "./lib/data-api";
+
+const icon = require("./assets/SPIcon.png");
 
 export default function App() {
+  const [books, setGames] = useState([]);
+
+  useEffect(() => {
+    getBooks().then((books) => {
+      setGames(books);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light"></StatusBar>
+      <Image
+        source={icon}
+        style={{
+          width: 100,
+          height: 200,
+          resizeMode: "center",
+        }}
+      />
+      <Button title="Bueno" onPress={() => alert(books.length)}></Button>
+
+      {books.map((book) => (
+        <View key={book.bookId} style={styles.card}>
+          <Text>{book.nombre}</Text>
+          <Text>{book.nombreAutor}</Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -13,8 +40,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    marginBottom: 10,
+    backgroundColor: "#060",
   },
 });
