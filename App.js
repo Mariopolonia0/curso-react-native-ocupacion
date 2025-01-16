1d4bbfd85715a540a7327aa87e53b5403cea4225
mario <mariopolonia0@gmil.com>
2025-01-15 22:03:06 -0500

    agregando conexion a api

--- App.js
+++ App.js
@@ -1,41 +1,52 @@
-import { useState } from "react";
+import React,{useState, useEffect} from 'react';
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, Button } from "react-native";
-import { getBooks } from "./lib/data-api";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableHighlight,
+} from "react-native";
+//"https://controltarea.azurewebsites.net/api/Books"
 
 const icon = require("./assets/SPIcon.png");
+const url = "https://controltarea.azurewebsites.net/api/Books";
 
 export default function App() {
-  const [books, setGames] = useState([]);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getBooks().then((books) => {
-      setGames(books);
-    });
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
   }, []);
+
 
   return (
     <View style={styles.container}>
-      <StatusBar style="light"></StatusBar>
-      <Image
-        source={icon}
-        style={{
-          width: 100,
-          height: 200,
-          resizeMode: "center",
-        }}
-      />
-      <Button title="Bueno" onPress={() => alert(books.length)}></Button>
-
-      {books.map((book) => (
-        <View key={book.bookId} style={styles.card}>
-          <Text>{book.nombre}</Text>
-          <Text>{book.nombreAutor}</Text>
-        </View>
-      ))}
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        
+        data.map((post) => {
+          return (
+            <View>
+              <Text style={styles.title}>{post.title}</Text>
+              <Text>{post.bookId}</Text>
+              <Text>{post.nombre}</Text>
+              <Text>{post.nombreAutor}</Text>
+            </View>
+          );
+        })
+      )}
     </View>
-  );
-}
+);}
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -49,3 +60,35 @@ const styles = StyleSheet.create({
     backgroundColor: "#060",
   },
 });
+
+  /* <View style={styles.container}>
+      <StatusBar style="light"></StatusBar>
+
+      <Image
+        source={icon}
+        style={{
+          width: 100,
+          height: 200,
+          resizeMode: "center",
+        }}
+      />
+      <View>
+        <Text>{"HOLA :" + book}</Text>
+      </View>
+      <Button
+        title="Bueno"
+        onPress={() => {
+          apiCall;
+        }}
+      ></Button>
+    </View>
+  );*/
+/*
+  {books.map((book) => (
+        <View key={book.bookId} style={styles.card}>
+          <Text>{book.nombre}</Text>
+          <Text>{book.nombreAutor}</Text>
+        </View>
+      ))}
+
+*/
